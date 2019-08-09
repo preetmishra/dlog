052d65c1244e2d5a7aa5004c1a3dd94d2e001269
preetmishra <ipreetmishra@gmail.com>
2019-08-10 04:38:37 +0530

    Add filter by tag feature

--- post/urls.py
+++ post/urls.py
@@ -6,4 +6,5 @@ app_name = 'post'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<str:slug>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('tag/<str:slug>/', views.FilterByTagView.as_view(), name='filter-by-tag'),
 ]
--- post/views.py
+++ post/views.py
@@ -4,15 +4,13 @@ from django.views.generic import (
     DetailView,
 )
 from post import models as post_models
+from django.urls import resolve
 
 # Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = 'post/index.html'
-
-    # def get_queryset(self):
-    #     return post_models.Entry.objects.filter(publish=True)
 
     def get(self, request, *args, **kwargs):
         entries = post_models.Entry.objects.filter(publish=True)
@@ -24,3 +22,22 @@ class IndexView(TemplateView):
 class PostDetailView(DetailView):
     template_name = 'post/post_detail.html'
     model = post_models.Entry
+
+
+class FilterByTagView(TemplateView):
+    template_name = 'post/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        tag = resolve(request.path_info).kwargs['slug']
+        tag_list = post_models.Tag.objects.filter(tags__isnull = False).distinct()
+
+        if tag == 'all':
+            tag = None
+            entries = post_models.Entry.objects.filter(publish = True)
+        else:
+            tag_id = post_models.Tag.objects.get(tag = tag).id
+            entries = post_models.Entry.objects.filter(publish = True).filter(tags = tag_id)
+
+        return render(request, self.template_name, {'entries': entries,
+                                                    'tag_list': tag_list,
+                                                    'tag_selected': tag})
