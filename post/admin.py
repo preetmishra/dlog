49b04ef7075ad48d26be14b11b6aec3080ee5ac8
preetmishra <ipreetmishra@gmail.com>
2019-08-09 15:47:31 +0530

    Add models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from post import models as post_models
 # Register your models here.
+
+@admin.register(post_models.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created', 'publish']
+    list_editable = ['publish']
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(post_models.Tag)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    tag = models.SlugField(max_length = 256, unique = True)
+
+    class Meta:
+        ordering = ['tag']
+
+    def __str__(self):
+        return self.tag
+
+class Entry(models.Model):
+    title = models.CharField(max_length = 256)
+    body = models.TextField()
+    slug = models.SlugField(max_length = 256, unique = True)
+    publish = models.BooleanField(default = True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta :
+        verbose_name_plural = "Entries"
+        ordering = ['-date_created']
+
+    def __str__(self) :
+        return self.title
+
+    
