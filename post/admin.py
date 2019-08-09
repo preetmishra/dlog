085980986c87b466e878889f06a6d8106b78a94a
preetmishra <ipreetmishra@gmail.com>
2019-08-09 22:16:16 +0530

    Add markdown

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.db import models
 from post import models as post_models
+from martor.widgets import AdminMartorWidget
+
 # Register your models here.
 
 @admin.register(post_models.Entry)
 class EntryAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
     list_display = ['title', 'date_created', 'publish']
     list_editable = ['publish']
     prepopulated_fields = {"slug": ("title",)}
