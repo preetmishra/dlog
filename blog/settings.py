2ae0a4f073e0f1a832e4f750d856aa38280e68f2
preetmishra <ipreetmishra@gmail.com>
2019-08-10 06:13:31 +0530

    Turn off debug

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('BLOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'wickedmishra.herokuapp.com', 
